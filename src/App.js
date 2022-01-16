7e54b497bac219f60cc4fb613cd9368394918e92
novusjendral <yanceblog123@gmail.com>
2022-01-16 07:40:00 +0000

    asu

---
+++ src/App.js
@@ -0,0 +1,36 @@
+
+//import './App.css';
+import "./Assets/App.scss"
+
+function App() {
+
+  return (
+   <div className='container'> 
+
+          <div className='header'> Header </div>
+          <div className='Section'>
+              <b>Menu</b> 
+            <div className='Card Section-body-1'>1</div>
+            <div className='Card Section-body-2'>2</div>
+            <div className='Card Section-body-3'>3</div>
+            <div className='Card Section-body-4'>4</div>
+            <div className='Card Section-body-5'>5</div>
+            <div className='Card Section-body-6'>6</div>
+            </div>
+        <div className='Konten'>
+            <div className='news Berita-1'>1</div>
+            <div className='news Berita-2'>
+                <div>Lorem Ipsum</div>
+                <div>Lorem Woaw</div>
+            </div>
+            <div className='news Berita-3'>
+                <div>Waw</div>
+                <div>Esek</div>
+                <div>Woakwokoakwoak</div>
+            </div> 
+        </div>
+    </div>
+  );
+}
+
+export default App;
